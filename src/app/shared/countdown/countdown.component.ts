d90f1b1fa89e03ca77fede013d8a8328b0aef2bd
Saban Uenlue - netTrek <us@netTrek.de>
2019-08-22 11:40:29 +0200

    ngIf

--- src/app/shared/countdown/countdown.component.ts
+++ src/app/shared/countdown/countdown.component.ts
@@ -1,24 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'dvz-countdown',
+@Component ( {
+  selector   : 'dvz-countdown',
   templateUrl: './countdown.component.html',
-  styleUrls: ['./countdown.component.scss']
-})
-export class CountdownComponent implements OnInit {
+  styleUrls  : [ './countdown.component.scss' ]
+} )
+export class CountdownComponent implements OnInit, OnDestroy {
 
   width = 100;
 
-  constructor() { }
+  private intervalID: number;
+
+  constructor() {
+  }
 
   ngOnInit() {
-    let intervalID: number;
-    intervalID = window.setInterval( () => {
+    this.intervalID = window.setInterval ( () => {
       this.width -= 10;
       if ( this.width === 0 ) {
-        window.clearInterval( intervalID );
+        this.stopInterval ();
       }
     }, 1000 );
+  }
+
+  ngOnDestroy(): void {
+    this.stopInterval ();
+  }
+
+  private stopInterval() {
+    if ( this.intervalID ) {
+      window.clearInterval ( this.intervalID );
+      this.intervalID = undefined;
+    }
   }
 
 }

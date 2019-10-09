61a90180254f7d247ecab5e2dc484415f9072a73
Saban Uenlue - netTrek <us@netTrek.de>
2019-10-09 16:25:41 +0200

    Komponente style ng-deep host encap

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'dvz-user',
   templateUrl: './user.component.html',
+  // styles: [ `h1 {
+  //   color: red;
+  // }`]
   styleUrls: ['./user.component.scss']
+  // encapsulation: ViewEncapsulation.ShadowDom
+  // encapsulation: ViewEncapsulation.None
 })
 export class UserComponent implements OnInit {
-
-  pause = 'ja bitte';
-
+  name = 'Saban Ünlü';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeName() {
+    const hello = 'world';
+    this.name = `${hello} Peter Müller`;
+  }
 }
 

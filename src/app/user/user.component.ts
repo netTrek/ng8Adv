912a52569cf58c9b15ae7307e78b06f1826525d1
Saban Uenlue - netTrek <us@netTrek.de>
2019-10-09 16:42:34 +0200

    Bindung - Ausdruck

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,26 +1,35 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
-@Component({
-  selector: 'dvz-user',
+@Component ( {
+  selector   : 'dvz-user',
   templateUrl: './user.component.html',
   // styles: [ `h1 {
   //   color: red;
   // }`]
-  styleUrls: ['./user.component.scss']
+  styleUrls  : [ './user.component.scss' ]
   // encapsulation: ViewEncapsulation.ShadowDom
   // encapsulation: ViewEncapsulation.None
-})
+} )
 export class UserComponent implements OnInit {
   name = 'Saban Ünlü';
+  width = 100;
+  height = 100;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
   changeName() {
     const hello = 'world';
-    this.name = `${hello} Peter Müller`;
+    this.name   = `${hello} Peter Müller`;
+    this.width = this.height = 300;
+  }
+
+  getName(): string {
+    console.log ( 'getName' );
+    return this.name;
   }
 }
 

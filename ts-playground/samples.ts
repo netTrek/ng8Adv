5875f803a097e822b3fea67a373c386288a620f8
Saban Uenlue - netTrek <us@netTrek.de>
2019-12-10 11:27:44 +0100

    sample 4 ts and json-server installation

---
+++ ts-playground/samples.ts
@@ -0,0 +1,184 @@
+// http://www.typescriptlang.org/play/?noImplicitAny=false&strictNullChecks=false&alwaysStrict=false&target=1&ssl=1&ssc=1&pln=182&pc=19#code/MYewdgzgLgBAJgQygiBTWBeGBvAUDAmUAGwEYAuGAcgAsFjiQqAafQkgJkqoHdVioLNgRIBmbhAQAjBGCGEiIYgBZuAB3SoATvPZKArNzqQquAL65coSLGyLiHZveUwzMLImRooAbkvXoGCktWTh3agAhAFkAdSo-ANgoAE8NcKpSUgAOOHjcAHoAKitwQOAELUo7YNDKaC0ASzAAcycUjTqoRpbXcLwFGrA4SkG4NtTUSnbUc1xC-JKbIgqsapCh8bSzP0XA4gboSjAAVwBbKW0AbQBdcMvSJ0cYUSdlJ31rhNLYYEYwVAAMgcoEczhctDc7gA6GH7aCfXawVAADygqCGQOgpDuAAYnDCoXCoAjEjAUWiMcCOLj8bDgU4AGxOADsTiynxg+XyMBxAFosv5vjANFoIOAqjAwAhTpMYPUmq0YAhmrKTudtL1VsJJdLZVRJDI5E5lbLSBxZqSRWLIJiQTgdTLOt1FSbQeqtK5IVhLtq7FLHdQDbIWEqVZQza5WAo-bqJNJZBwQ66YBxqWZWNdBUsreAAMJ-VB9GAEnNgVxfJYgABu2kacFQAAVtNaiyXm+BjWGU2mK4Fyei4E3ReBWzDS04AF4NNSUZQMjhZHuWLkwAA8UmOUCgI4acAwACIpFAwPuYOBfg1gABrA-GODEbQACgAlPuAHwFbkbrfgKHn-bXuEABmxxgMAUANCOd4Ph6L72gSUIwFANAHBYvwoBAMAAHIIFWETHjg2pHmAOEBvKLR+AoK5qI0VZIIWxGUAAEgAKlEAIRJu25gAAog+MpgFA2qJFoxzgSAWiPjRDR0WiQTHsxbEcVx4B8agAlQM+hEKFR3LIQcULEeExGUTpBD6RAUJNA0UAvqZBAWAo0myYW1m2Vp-RmUhKGWcRUIIHAcA8TWgm2uiT5UBe14hjApwgMcaCoCFmBvtpXkKAESioISIDNDAj6RQBV6oLkbQ+YZx6kagThxQlqBJeiUBQhejUABowM+2oKGYnXdRaQpSnhCnYbh+Fllg-w8CNQ1gI+2pwCAwBnI1UIqlAakaREyQAJJhAVxFUFpKAwKx7GcT+vH8Y1uCdcuxQgWBEEjg9wCkHBnkiKUWU5c0BUIP9h1+BYiIwC91JYC9T1lu9wlfQ+P2PmaojPkDWaBC9ojhJNMAAGKgeBkGzZFcPZYweWPhwojKFpgNzAsIMvdiEP41D+UebDkDfWTiOUyjswgxUWggDwTNs+4qUfYonPw9zSN88DDP4+DoMs4T+XChU0pOgqHVpZ90uk7lj4ayEpwdajAtaELPDK1JmunNrLROGo9tcHKXQKlpGASxzYoy0bJvSub-OkoLwscAA+lWysu6b4v2iuJwetisdByAND-EhEy+1zAep2b8to7AAVwPowGqyOdum+zCgNEB+XTCA9f5+4GBYPqHstIdes6Vo6DHFoZYtwA1NQeTdWSxBoD3Ch91AA9lvo2oWMDoeBfoACCVvC+ELfe4HZuj-oOykmigRYCXm-bzwiN87gTRoloQEIMAhbbQAqmgHqS-6srkc09lQyqjBNoQBK41obxVC+N04J+bSHqC-H4xAMInTOLIHuK4eINBVJAYAdAgIUm1InWM7tnSAJEmJbckkuqEDUMcKQAEHR-07q0GhBBnL0RgHwbBNA7RqlgQoGuXkVwWShL-LGupAGOUICuKI6AaAgHrGAbUkhkgAE14paCqtA0hOtJYZRJgjKgl4aBBCaCGURv8+bpTngvbyBlf5SItMgiAmEojoP7EMTCTE0FlgaKcNQV1BKYQ-l-HuHC5IR2TPw0BlgFDeCAY+aJICtBCLMqIqJKpwgmicQoNaiTnwwI1PowgtjB72Mspk1AuS9A2FEuJSSv9HaKm4c0XhRTUkz0IBAY4IpHy-ycK03hwc+rxIQOozR2jCm6J6CUggK4yllioDRVAQEGjIioDAUeljdRsJgIsuUvTtBQlURogeUyakEDWmcrQkDUDG3rAIBAHTwh4gPixVAhxJQpI6i8uZ+z+7lNESaLZ8B+DIEubFeRijUAAHk1BQAbPbKu0oPlfJiVoJwjzkAAH4OlpJ0nXfKABCA4OEsL5WxQgZ8BL0p7OkQ5bUMpkIwoAEqfMRfbCAKLTgUG+e6KMXl85uwxYKsy+dxD8vBGKnSBIGiGg6TcbUtLCDLxDkKU4shKDuPGpKVAU0dUFSDEaGApAcQ4j5pqsA-kslYFEKIHwnJuTeEfrsXOv0rU2tQCjJ1MA1qPx2JYB8sA6qVBgKEjURQYxkRYZ2YB7pXDzD6NqJpgYWG6AIMmZQyh+bBvgPRSgAARThE19UwGLWiOyQb0AwFDam-+6RjUwAAD9gGIM2vIebJpVUbemk+A0zjhHcchKEDZto7DzU29uNB+CMDyEBCS+U80NFeY6ldq5TU4jXcPYeBLMr+1+g0axhAF2wTzQAK1XTAS9G6zWOvPTulV+s-aG0Pcesy+7X2PnPe+1wswVyfoRkenwQA
+
+const dataset = {
+  col1: 'hallo',
+  col2: 'welt',
+  col3: 'saban',
+  col4: 'peter',
+  col5: 'hans'
+}
+
+const { col2, col4 } = dataset;
+
+const brand = 'BMW';
+const type = '118d';
+/*
+const car: { brand: string, type: string } = {
+    brand: brand, type: type
+}
+*/
+const car= { brand, type };
+
+const list: number[] = [1, 2, 3, 4, 5];
+const cloneList: number[] = [...list];
+const extendList1 = [0, ...list];
+const extendList2 = [0, ...list, 6, 7, 8]; // 0-8
+
+const person: { name: string, age: number } = {
+  name: 'saban', age: 12
+}
+const personsList: { name: string, age: number }[] = [
+  { name: 'saban', age: 12 },
+  { name: 'saban2', age: 22 },
+]
+
+const personClone = { ...person };
+const overridePerson = { ...person, age: 22 };
+const extendPerson = { ...person, zip: 46282 };
+
+// <button id="btn" onclick="handler()">
+// button.onclick = function handler () { .... this}
+class NavBtn {
+  btnName: string;
+  // private btn: HTMLButtonElement
+  constructor(private btn: HTMLButtonElement) {
+    // this.btn = btn;
+    this.init();
+  }
+  private init() {
+    this.btn.addEventListener('click',  mouseevent => {
+      console.log ('clicked', this.btnName, mouseevent.clientX )
+    })
+  }
+}
+const navBtn: NavBtn = new NavBtn(
+  document.getElementById ('btn') as HTMLButtonElement
+)
+
+
+/*
+function func1() {
+    console.log('aaa');
+}
+
+const func2 = function () {
+    console.log(123);
+}
+
+const func3 = new Function('console.log (234) ');
+*/
+
+const func1 = function () {
+  console.log(123);
+}
+
+const arrow1 = () => {
+  console.log(123);
+}
+
+
+const func2 = function ( param: string ) {
+  console.log( param );
+}
+
+const arrow2 = (param: string, param2: string) => {
+  console.log( param );
+}
+
+const arrow2_v2 = param => { // nur 1 param ohne type
+  console.log( param );
+}
+
+const add5 = function (param) {
+  if (typeof param === 'string') {
+    return param + '';
+  } else {
+    return 5
+  }
+}
+
+const add5Arrow = param => param + 5;
+
+const test = add5Arrow(1);
+
+interface IUser {
+  name: string;
+  age: number;
+  // getAge(): number
+}
+
+abstract class Human {
+  // Eigenschaften
+  // name: string;
+  constructor(
+    public name: string,
+    private weight: number
+  ) {
+    // this.name = name;
+  }
+  // Methoden
+  sayYourName(): string {
+    console.log('ich bin', this.name);
+    return this.name;
+  }
+}
+class Man extends Human implements IUser {
+  private _age: number;
+
+  set age(age: number) {
+    this._age = age;
+  }
+  get age(): number {
+    return this._age;
+  }
+  constructor(name: string, weight: number) {
+    super(name, weight );
+  }
+  sayYourName(): string {
+    // return 'prefix' + this.name
+    return super.sayYourName();
+  }
+  getYourAge( delta: number = 0, paramTest: number ): number {
+    return this.age + delta;
+  }
+  methodeOptParam(paramTest: number, delta?: number) {
+    if (!isNaN (delta)) {
+
+    }
+  }
+  methodeRestParams(param1: number,
+                    param2: number,
+                    param3: number,
+                    ...iban: number[]
+  ) {
+
+  }
+}
+
+const man: Man = new Man('saban', 100);
+man.age = 33; // setter
+console.log(man.age); // getter;
+
+
+
+let user: IUser /*{ name: string, age: number }*/ = {
+  name: 'string',
+  age: 44
+}
+
+let date: Date = new Date();
+
+let username: string = 'saban ünlü';
+let newName = 'string';
+
+const num = Math.PI;
+
+let saban = 'hello';
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+  for (let j = 0; j < 10; j++) {
+    console.log(i);
+    console.log(j);
+  }
+}
+// console.log(i);
